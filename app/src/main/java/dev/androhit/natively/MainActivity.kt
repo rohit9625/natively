45730dd4230fbb1f312c3eaf72c424e85cda0450
Rohit Verma <rv17837@outlook.com>
2026-02-17 20:39:12 +0530

    chore: setup app color theme and typography

--- app/src/main/java/dev/androhit/natively/MainActivity.kt
+++ app/src/main/java/dev/androhit/natively/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NativelyTheme {
+            NativelyTheme(dynamicColor = false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
