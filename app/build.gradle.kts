d7687612109cf1b9e8ef7a119872558bcf824fa2
Rohit Verma <rv17837@outlook.com>
2026-02-20 11:51:38 +0530

    chore: add camera dependencies and declare permissions for camera access

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,13 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
+    implementation(libs.androidx.camera.mlkit.vision)
+
     implementation(platform(libs.koin.bom))
     implementation(libs.koin.android)
     implementation(libs.koin.compose)
