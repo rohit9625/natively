091cf93723e99a8840846847dea78bc2d91ca4d9
Rohit Verma <rv17837@outlook.com>
2026-02-23 00:41:06 +0530

    chore: add datastore preferences library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
 
     implementation(libs.kotlinx.serialization.core)
     implementation(libs.slf4j.android)
+    implementation(libs.androidx.datastore.preferences)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
